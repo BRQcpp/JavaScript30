83da337d98b5051aa275c2be345a68d788c1e347
BRQ <bnowak4744@gmail.com>
2022-02-01 17:41:15 +0100

    Audio buttons are now clickable

--- 01 - JavaScript Drum Kit/audio.js
+++ 01 - JavaScript Drum Kit/audio.js
@@ -1,16 +1,35 @@
-document.addEventListener('keypress', (e) => { playSound(e); });
+let buttons = document.querySelectorAll('.audiobuttons');
 
-function playSound(e) {
+buttons.forEach( button =>
+{
+    button.addEventListener('click', () => 
+    {
+        playAudioClick(button);
+    } );
+})
+
+function playAudioClick(button) {
+    let charCode = button.getAttribute('data-key');
+    audio = document.querySelector(`audio[data-key="${charCode}"]`);
+    playWithEffect(button, audio);
+}
+
+  
+document.addEventListener('keypress', (e) => { playAudioKeyPress(e); });
+
+function playAudioKeyPress(e) {
     let audio = document.querySelector(`audio[data-key="${e.key.toUpperCase().charCodeAt()}"]`);
     
     if(audio == null) 
         return undefined;
 
-    let button = document.querySelector(`div[data-key="${e.key.toUpperCase().charCodeAt()}"]`);
+    let button = document.querySelector(`.audiobuttons[data-key="${e.key.toUpperCase().charCodeAt()}"]`);
+    playWithEffect(button, audio);
+}
+
+function playWithEffect(button, audio) {
     button.classList.add('pressed');
-    
     document.addEventListener('transitionend', () => { button.classList.remove("pressed");} );
-    
     audio.currentTime = 0;
     audio.play();
 }
